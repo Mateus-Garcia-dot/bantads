5bed0fbd68cc3d3f33e79be0258a41190948f7bc
Fernanda Costa <fernanda.costa@objective.com.br>
2022-12-20 17:10:42 -0300

    correção detalhes cliente

--- src/app/gerente/gerente-cliente-detalhes/gerente-cliente-detalhes.component.ts
+++ src/app/gerente/gerente-cliente-detalhes/gerente-cliente-detalhes.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 import { ClienteService } from 'src/app/cliente/services/cliente.service';
+import { ContaService } from 'src/app/cliente/services/conta.service';
 import { Cliente } from 'src/shared/models/cliente.model';
 
 @Component({
@@ -11,18 +13,27 @@ import { Cliente } from 'src/shared/models/cliente.model';
 export class GerenteClienteDetalhesComponent implements OnInit {
   
   @Input() clienteId?: number;
-  cliente!: Cliente;
+  @Input() cliente!: Cliente;
 
   constructor(
     private clienteService: ClienteService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private contaService: ContaService
   ) {}
 
   ngOnInit(): void {
     console.log(this.clienteId)
     let id = +this.route.snapshot.params['id'] || this.clienteId!;
-    this.clienteService.buscarClientePorId(id).subscribe(c => {
-      this.cliente = c;
+    this.clienteService
+    .buscarClientePorId(id)
+    .pipe(switchMap(cliente => {
+      this.cliente = cliente
+      return this.contaService.buscarContaPorCliente(this.cliente.id)
+    }
+    ))
+    .subscribe(conta => {
+      console.log(conta)
+      this.cliente!.conta = conta[0];
     })
 
   }
