e29c1e21de6e830b17ed7e28a300a91b25e0c58c
Mateus Medeiros Garcia <mateus.medeiros@ufpr.br>
2023-02-21 16:26:35 -0300

    add qunatidade de clientes

--- src/app/admin/admin-home/admin-home.component.ts
+++ src/app/admin/admin-home/admin-home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CrudContaService } from 'src/app/conta/services/crud-conta.service';
-import { CrudGerenteService } from 'src/app/gerente/services/crud-gerente.service';
 import db from 'src/app/shared/database/database';
 import { Gerente } from 'src/app/shared/models/gerente.model';
 
@@ -14,6 +12,7 @@ export class AdminHomeComponent implements OnInit {
 
   positiveBalances!: Array<number>;
   negativeBalances!: Array<number>;
+  qntClientes!: Array<number>;
 
   constructor() { }
 
@@ -21,11 +20,13 @@ export class AdminHomeComponent implements OnInit {
     this.gerentes = []
     this.positiveBalances = [];
     this.negativeBalances = [];
+    this.qntClientes = [];
     const datas = await db.get('/manager/account');
     for (let data of datas.data) {
       this.gerentes.push(new Gerente(data.uuid, data.name, data.cpf, data.telephone, data.authentication))
       this.positiveBalances.push(data.sumPositiveBalance);
       this.negativeBalances.push(data.sumNegativeBalance)
+      this.qntClientes.push(data.count)
     }
   }
 }
